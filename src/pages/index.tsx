e69e75d606e45d5af091e97845f39e74840d76cb
Hocky Yudhiono <hocky.yudhiono@gmail.com>
2022-11-07 09:20:22 +0700

    Change title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,8 +12,8 @@ const Index = () => {
     <Main
       meta={
         <Meta
-          title="Next.js Boilerplate Presentation"
-          description="Next js Boilerplate is the perfect starter code for your project. Build your React application with the Next.js framework."
+          title="QS World University Ranking Semantic Web"
+          description="QS World University Ranking Semantic Web"
         />
       }
     >
