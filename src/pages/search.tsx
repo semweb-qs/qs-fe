20b888e7a8a5bff6fbc72c1d98fd21f19acdcf2f
Hocky Yudhiono <hocky.yudhiono@gmail.com>
2022-12-04 06:04:26 +0700

    Add excerpts

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 
@@ -6,6 +7,9 @@ import SearchBar from '@/components/SearchBar';
 import SearchResultComponent from '@/components/SearchResultComponent';
 import { Meta } from '@/layouts/Meta';
 import { Main } from '@/templates/Main';
+
+const BASE_API = 'http://localhost:8080';
+const SEARCH_API = `${BASE_API}/search`;
 
 const images = [
   {
@@ -34,7 +38,7 @@ const accordions = [
   { header: 'Ini', content: 'Kocak lu' },
 ];
 
-const Search = () => {
+const Search = ({ searchResult }) => {
   const router = useRouter();
   const [textFieldValue, setTextFieldValue] = useState('');
   const q = String(router.query.q);
@@ -54,41 +58,16 @@ const Search = () => {
             id="search-result"
             className="flex flex-col gap-7 w-full md:w-3/5 m-2 px-3"
           >
-            <SearchResultComponent
-              title={description}
-              desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed elementum nisl et orci feugiat semper. Aliquam erat volutpat. Maecenas semper ipsum a dolor interdum, a accumsan risus malesuada. Cras eu bibendum massa. Nunc suscipit quam vel est condimentum, ut posuere ante ultricies. Curabitur porttitor, augue a volutpat dictum, metus dolor commodo odio, ultricies viverra lorem risus id nisi. Aenean mollis, libero et dapibus vulputate, ligula metus facilisis urna, ac dignissim justo eros id ex. Donec interdum molestie velit nec aliquam. Ut tempor tincidunt luctus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec aliquet augue mauris, sed tempus tellus viverra vitae. Nunc luctus in ante id bibendum. Nunc vehicula dui et ornare volutpat. Ut gravida venenatis odio sit amet consectetur. Praesent tempus varius gravida. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
-              url="https://example.org/vocab/mit"
-            ></SearchResultComponent>
-            <SearchResultComponent
-              title="MIT"
-              desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed elementum nisl et orci feugiat semper. Aliquam erat volutpat. Maecenas semper ipsum a dolor interdum, a accumsan risus malesuada. Cras eu bibendum massa. Nunc suscipit quam vel est condimentum, ut posuere ante ultricies. Curabitur porttitor, augue a volutpat dictum, metus dolor commodo odio, ultricies viverra lorem risus id nisi. Aenean mollis, libero et dapibus vulputate, ligula metus facilisis urna, ac dignissim justo eros id ex. Donec interdum molestie velit nec aliquam. Ut tempor tincidunt luctus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec aliquet augue mauris, sed tempus tellus viverra vitae. Nunc luctus in ante id bibendum. Nunc vehicula dui et ornare volutpat. Ut gravida venenatis odio sit amet consectetur. Praesent tempus varius gravida. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
-              url="https://example.org/vocab/mit"
-            ></SearchResultComponent>
-            <SearchResultComponent
-              title="MIT"
-              desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed elementum nisl et orci feugiat semper. Aliquam erat volutpat. Maecenas semper ipsum a dolor interdum, a accumsan risus malesuada. Cras eu bibendum massa. Nunc suscipit quam vel est condimentum, ut posuere ante ultricies. Curabitur porttitor, augue a volutpat dictum, metus dolor commodo odio, ultricies viverra lorem risus id nisi. Aenean mollis, libero et dapibus vulputate, ligula metus facilisis urna, ac dignissim justo eros id ex. Donec interdum molestie velit nec aliquam. Ut tempor tincidunt luctus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec aliquet augue mauris, sed tempus tellus viverra vitae. Nunc luctus in ante id bibendum. Nunc vehicula dui et ornare volutpat. Ut gravida venenatis odio sit amet consectetur. Praesent tempus varius gravida. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
-              url="https://example.org/vocab/mit"
-            ></SearchResultComponent>
-            <SearchResultComponent
-              title="MIT"
-              desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed elementum nisl et orci feugiat semper. Aliquam erat volutpat. Maecenas semper ipsum a dolor interdum, a accumsan risus malesuada. Cras eu bibendum massa. Nunc suscipit quam vel est condimentum, ut posuere ante ultricies. Curabitur porttitor, augue a volutpat dictum, metus dolor commodo odio, ultricies viverra lorem risus id nisi. Aenean mollis, libero et dapibus vulputate, ligula metus facilisis urna, ac dignissim justo eros id ex. Donec interdum molestie velit nec aliquam. Ut tempor tincidunt luctus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec aliquet augue mauris, sed tempus tellus viverra vitae. Nunc luctus in ante id bibendum. Nunc vehicula dui et ornare volutpat. Ut gravida venenatis odio sit amet consectetur. Praesent tempus varius gravida. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
-              url="https://example.org/vocab/mit"
-            ></SearchResultComponent>
-            <SearchResultComponent
-              title="MIT"
-              desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed elementum nisl et orci feugiat semper. Aliquam erat volutpat. Maecenas semper ipsum a dolor interdum, a accumsan risus malesuada. Cras eu bibendum massa. Nunc suscipit quam vel est condimentum, ut posuere ante ultricies. Curabitur porttitor, augue a volutpat dictum, metus dolor commodo odio, ultricies viverra lorem risus id nisi. Aenean mollis, libero et dapibus vulputate, ligula metus facilisis urna, ac dignissim justo eros id ex. Donec interdum molestie velit nec aliquam. Ut tempor tincidunt luctus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec aliquet augue mauris, sed tempus tellus viverra vitae. Nunc luctus in ante id bibendum. Nunc vehicula dui et ornare volutpat. Ut gravida venenatis odio sit amet consectetur. Praesent tempus varius gravida. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
-              url="https://example.org/vocab/mit"
-            ></SearchResultComponent>
-            <SearchResultComponent
-              title="MIT"
-              desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed elementum nisl et orci feugiat semper. Aliquam erat volutpat. Maecenas semper ipsum a dolor interdum, a accumsan risus malesuada. Cras eu bibendum massa. Nunc suscipit quam vel est condimentum, ut posuere ante ultricies. Curabitur porttitor, augue a volutpat dictum, metus dolor commodo odio, ultricies viverra lorem risus id nisi. Aenean mollis, libero et dapibus vulputate, ligula metus facilisis urna, ac dignissim justo eros id ex. Donec interdum molestie velit nec aliquam. Ut tempor tincidunt luctus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec aliquet augue mauris, sed tempus tellus viverra vitae. Nunc luctus in ante id bibendum. Nunc vehicula dui et ornare volutpat. Ut gravida venenatis odio sit amet consectetur. Praesent tempus varius gravida. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
-              url="https://example.org/vocab/mit"
-            ></SearchResultComponent>
-            <SearchResultComponent
-              title="MIT"
-              desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed elementum nisl et orci feugiat semper. Aliquam erat volutpat. Maecenas semper ipsum a dolor interdum, a accumsan risus malesuada. Cras eu bibendum massa. Nunc suscipit quam vel est condimentum, ut posuere ante ultricies. Curabitur porttitor, augue a volutpat dictum, metus dolor commodo odio, ultricies viverra lorem risus id nisi. Aenean mollis, libero et dapibus vulputate, ligula metus facilisis urna, ac dignissim justo eros id ex. Donec interdum molestie velit nec aliquam. Ut tempor tincidunt luctus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec aliquet augue mauris, sed tempus tellus viverra vitae. Nunc luctus in ante id bibendum. Nunc vehicula dui et ornare volutpat. Ut gravida venenatis odio sit amet consectetur. Praesent tempus varius gravida. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
-              url="https://example.org/vocab/mit"
-            ></SearchResultComponent>
+            {searchResult.map((val, idx) => {
+              return (
+                <SearchResultComponent
+                  key={idx}
+                  title={val.id}
+                  desc={val.excerpt}
+                  url={val.path}
+                ></SearchResultComponent>
+              );
+            })}
           </div>
         </div>
       </Main>
@@ -105,9 +84,14 @@ export async function getServerSideProps(context) {
       },
     };
   }
-
+  const res = await axios.post(SEARCH_API, {
+    content: context.query.q,
+    rerank: true,
+  });
   return {
-    props: {},
+    props: {
+      searchResult: res.data.results,
+    },
   };
 }
 
