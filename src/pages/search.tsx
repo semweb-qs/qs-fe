d2c91f6094024c65f217239b901157d458c45c1e
Hocky Yudhiono <hocky.yudhiono@gmail.com>
2022-12-04 08:44:15 +0700

    Add fe

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -11,33 +11,6 @@ import { AppConfig } from '@/utils/AppConfig';
 
 const SEARCH_API = `${AppConfig.base_backend}/search`;
 
-const images = [
-  {
-    src: 'https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg',
-  },
-  {
-    src: 'https://static.wikia.nocookie.net/gensin-impact/images/e/e1/Character_Venti_Game.png',
-  },
-  {
-    src: 'https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg',
-    alt: 'Boats (Jeshu John - designerspics.com)',
-  },
-  {
-    src: 'https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg',
-  },
-];
-
-const attributes = [
-  ['Nama', 'Massachusets Institute of Technology'],
-  ['Lokasi', 'Depok'],
-  ['Negara', 'Indonesia'],
-];
-
-const accordions = [
-  { header: 'Ini kepalanya', content: 'Ini Kontennya' },
-  { header: 'Ini', content: 'Kocak lu' },
-];
-
 const Search = ({ searchResult }) => {
   const router = useRouter();
   const [textFieldValue, setTextFieldValue] = useState('');
