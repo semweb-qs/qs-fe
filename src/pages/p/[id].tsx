ef4f34d3e046d3541f7e063961330ab04d8d8b3c
Hocky Yudhiono <hocky.yudhiono@gmail.com>
2022-12-20 16:54:53 +0700

    Add p property

---
+++ src/pages/p/[id].tsx
@@ -0,0 +1,47 @@
+import { useRouter } from 'next/router';
+import { useCallback } from 'react';
+import Particles from 'react-tsparticles';
+import { loadFull } from 'tsparticles';
+import type { Engine } from 'tsparticles-engine';
+
+import BoxComponent from '@/components/BoxComponent';
+import { Meta } from '@/layouts/Meta';
+import { Main } from '@/templates/Main';
+import { particlesConfig } from '@/utils/particlesConfig';
+
+const PropertyBase = () => {
+  const router = useRouter();
+  const particlesInit = useCallback(async (engine: Engine) => {
+    await loadFull(engine);
+  }, []);
+  const vocabID = router.query.id;
+
+  return (
+    <Main
+      meta={
+        <Meta
+          title="QS World Search Engine"
+          description="QS World Search Engine Homepage"
+        />
+      }
+    >
+      <Particles
+        className={'z-[-1] fixed'}
+        id="tsparticles"
+        init={particlesInit}
+        /*
+            // @ts-ignore */
+        options={particlesConfig}
+      />
+      <div
+        className={
+          'flex w-[70vw] max-w-xl justify-center content-center align-middle mx-auto my-10'
+        }
+      >
+        <BoxComponent boxID={vocabID} type={'PropertyBase Type'}></BoxComponent>
+      </div>
+    </Main>
+  );
+};
+
+export default PropertyBase;
